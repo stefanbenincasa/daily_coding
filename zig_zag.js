952471911db74f3a34de3b192f15addfce75bdee
Stefan Benincasa <stefan.benin@gmail.com>
2020-10-14 16:50:55 +1100

    solved zig_zag problem; simplified it down to only two rows, as 3+ was too complicated; will revisit at later date

--- zig_zag.js
+++ zig_zag.js
@@ -3,24 +3,11 @@
 const str = "qwerty"
 const array = str.split("")
 
-// Main
-let index, step, limit, output
+let firstRow, secondRow, output
 
-// Determine "limit" of the "step" which proceeds in 
-// increments relevant to desired row amount
-
-step = array.length / 2 - 1
-limit = array.length
-index = 0
-
-// Problem : every "console.log" prints to newline
-while(index < limit) {
-	if(index % 2 === 0) output += array[index] + " "
-	else output += " " + array[index]
-	index += step
-}
-
-/// Functions /// 
-function isWithinArrayLength(array, index) {
-	return index <= array.length - 1 ? true : false
+firstRow = ""; secondRow = "";
+for(let index = 0; index <= array.length; index++) {
+	if (index === array.length) console.log(firstRow + "\n" + secondRow)
+	else if(index % 2 === 0) firstRow += array[index] + " "
+	else secondRow += " " + array[index]
 }
