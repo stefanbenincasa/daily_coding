07c1ed63b44349dcb52b8ac1d6fdb45706ad0264
Stefan Benincasa <stefan.benin@gmail.com>
2020-10-15 17:57:10 +1100

    stalled at sorting algorithm; stock JS sort() method will suffice but will attempt custom ascending sorting algorithm

--- smallest_rotated_string.js
+++ smallest_rotated_string.js
@@ -4,15 +4,33 @@
 	at least one
 */
 
+
+// Globals
 const string = "string"
 const k = 2
 
-
-/* Rearrange word, placing k amount of starting characters to
-word rear */
-console.log(getShiftedWord())
+// Rearrange word, placing k amount of starting characters to word rear 
+let shiftedWord = getShiftedWord()
 
 // Lexigraphically order new word characters but for new suffix
+let lexiSorted = getLexiSorted(shiftedWord)
+
+// Log
+console.log(shiftedWord)
+
+/// Functions ///
+
+// Sort the prefix of the word by the unicode code value;
+// smallest to largest
+function getLexiSorted(shiftedWord) {
+
+	let portionToSort, lexiSorted
+
+	portionToSort = shiftedWord.substring(0, shiftedWord.length-k)
+	portionToSort = portionToSort.split("")
+	// Try to implement own ascending sorting algorithm
+	
+}
 
 function getShiftedWord() {
 
