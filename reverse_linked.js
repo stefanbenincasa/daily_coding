62e631e3d45283207acf0fbff6014339804777e8
Stefan Benincasa <stefan@Stefans-iMac.netgear.com>
2020-10-19 17:02:09 +1100

    attempt at solving reversed_linked functionally; attempting with pointers

--- reverse_linked.js
+++ reverse_linked.js
@@ -12,25 +12,53 @@ let list = {
 	}
 }
 
-console.log(getReversed(list))
+let reversed, linked
+reversed = getReversed(list.head)
+setLinked(reversed)
+console.log(linked)
 
-function getReversed(list) {
+function getReversed(head) {
 
-	let node, previous, head, tail, reversed 
-	head = list.head
+	let node, tail, array
+
+	array = []
+	tail = getTail(head.next ? head.next : null)
 	node = head
 
-	if(!head) return null
-	else if(!head.next) return head.value
+	if(!head||!head.next) return null 
 	else {
 
 		while(node.next) {
-			previous = node
+			array.unshift(node.value)
 			node = node.next
 		}
 
-		tail = node
+		array.unshift(tail.value)
 
 	}
 
+	return array
+
+}
+
+function setLinked(array) {
+
+	let object, objects
+
+	object = {}; objects = [];
+	for(let i = 0; i < array.length; i++) 
+		object.value = array[i].value
+		object.next = {}
+		objects.push(object)
+		object = {}
+
+	for(let i = 0; i < array.length - 1; i++) 
+		objects[i].next = objects[i+1]
+	
+}
+
+function getTail(node) {
+	if(!node) return null
+	else if(node.next) return getTail(node.next)
+	else return node
 }
